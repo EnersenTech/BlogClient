cb2161d5e57fdfdf6ed6b0d0dd6c51204009f530
Ubuntu <ubuntu@ip-172-31-11-6.ap-northeast-2.compute.internal>
2022-09-26 07:45:56 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      	<div>Tech Enersen's Contents</div>
+	  <div>Coming Soon!</div>
+	  </header>
+    </div>
+  );
+}
+
+export default App;
