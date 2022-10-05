f99136b85aa89f3f7d00ae2b503299642b38efca
Lewis <lewis@Lewiss-MacBook-Pro.local>
2022-10-05 19:14:29 +0900

    dev env add & image test

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,20 +1,26 @@
 import React, {useEffect, useState} from 'react'
+import './Projects.css'
 import {GetFetchQuotes} from '../../api/fetch'
+
 
 function Projects (){
 
-    useEffect(()=>{
-        GetFetchQuotes({
-            uri: `${process.env.REACT_APP_SERVER_IP}/uploads/Thumbnail_1.png`,
-            // uri: `${process.env.REACT_APP_LOCAL_SERVER_IP}/uploads/Thumbnail_1.png`,
-            msg: 'GET projects thumbnail'
-        }).then((response)=>{
-            console.log(response)
-        })
-    },[])
+    // const [imges, setImages] = useState('')
+
+    // useEffect(()=>{
+    //     GetFetchQuotes({
+    //         uri: `${process.env.REACT_APP_SERVER_IP}/uploads/Thumbnail_1.png`,
+    //         msg: 'GET projects thumbnail'
+    //     }).then((response)=>{
+    //         console.log(response)
+    //         const {data} = response
+    //         setImages(data)
+    //     })
+    // },[])
     return (
     <div>
         projects page
+        <img className='Thumbnail-size' src={`${process.env.REACT_APP_SERVER_IP}/uploads/Thumbnail_1.png`} />
     </div>
     )
 }
