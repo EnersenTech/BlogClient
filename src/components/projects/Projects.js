d28149128ca1d2effea3f7b04d705c2acca09c62
Lewis <lewis@Lewiss-MacBook-Pro.local>
2022-10-05 20:21:43 +0900

    lost gitflow update

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -6,7 +6,6 @@ import {GetFetchQuotes} from '../../api/fetch'
 function Projects (){
     return (
     <div>
-        projects page
         <img className='Thumbnail-size' src={`${process.env.REACT_APP_SERVER_IP}/uploads/Thumbnail_1.png`} />
     </div>
     )
